993c07881671648ab9221c2ac7f6ba5e7676c24b
Artem Mamichev <artem.mamichev@gmail.com>
2021-02-14 22:59:54 +0200

    deleted temp file

--- semester_2/lab2/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-void parse_input(string str, int* v, string* car_num){
-//     int i = 0;
-//     while (str[i] != ' '){
-//         *v = *v * 10 + str[i] + stoi(str);
-//         i++;
-//     }
-//     *car_num = str;
-// }
